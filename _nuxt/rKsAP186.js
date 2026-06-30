8ef93a46e0384047c1bf58935252fba11b6169c5
kopigreenx <mokhamad.rizkianto@gmail.com>
2026-06-30 16:05:28 +0700

    Updates

---
+++ _nuxt/rKsAP186.js
@@ -0,0 +1,1 @@
+import{r as a}from"./CnZn7EfN.js";import n from"./IKQtKMAA.js";import{a3 as s,a as t,p as e,s as i,o as p}from"./DbGnRlpb.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(p(),t(n,{color:"warning",icon:i(o).ui.icons.warning},{default:e(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
